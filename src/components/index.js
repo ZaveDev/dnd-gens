47d18e5d2118d463c676daafd4c6de2ed1c26527
zavedev <66022270+ZaveDev@users.noreply.github.com>
2022-03-24 18:30:57 -0400

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Settlements from "./Settlements";
-import Nav from "./Nav";
-import MagicItems from "./MagicItems";
-import Parcel from "./Parcel";
-import Home from './Home'
-export { Settlements, Nav, MagicItems, Parcel, Home };
--- src/utils/modules/loot/hoardC.js
+++
@@ -1,7 +0,0 @@
-import { roll100 } from "../../roll";
-export const hoardC = () => {
-  const x = roll100();
-  let res = x;
-
-  return res;
-};
